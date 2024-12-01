04505f357796e28c870dc5597b6c2ce61337238e
ismet bayandur <31832577+ismetba@users.noreply.github.com>
2024-12-01 03:02:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function colonizeIt(element) {
-    element.click();
-    await sleep(500);
-    colonizeBtn.click();
-}
-
 (function reloadScript() {
-    
+    console.log("Script çalışıyor!");
+
     // Sayfa yenilendiğinde script tekrar çalışsın
     setTimeout(() => {
-        if (js_cityLocation0Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation0);
-        else if (js_cityLocation1Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation1);
-        else if (js_cityLocation2Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation2);
-        else if (js_cityLocation3Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation3);
-        else if (js_cityLocation4Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation4);
-        else if (js_cityLocation5Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation5);
-        else if (js_cityLocation6Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation6);
-        else if (js_cityLocation7Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation7);
-        else if (js_cityLocation8Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation8);
-        else if (js_cityLocation9Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation9);
-        else if (js_cityLocation10Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation10);
-        else if (js_cityLocation11Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation11);
-        else if (js_cityLocation12Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation12);
-        else if (js_cityLocation13Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation13);
-        else if (js_cityLocation14Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation14);
-        else if (js_cityLocation15Link.title == "Burayı kolonileştirmek istediğine emin misin?")
-            colonizeIt(cityLocation15);
-        else
-            location.reload(); // Sayfayı yenile
+        console.log("Sayfa yenileniyor...");
+        location.reload(); // Sayfayı yenile
     }, 5000); // 5 saniye sonra
 
     // Kendini tekrar sayfaya ekle (self-injection)
