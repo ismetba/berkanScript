58eb88b60f9ee4b84f59da253160229b487d7468
ismetba <ismet3170766@gmail.com>
2024-12-03 23:26:41 +0300

    17

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
 (function reloadScript() {
-    if (window.__isReloading) {
-        console.log("Script zaten çalışıyor, yeniden başlatılmıyor.");
-        return;
-    }
-    window.__isReloading = true;
 
-    console.log("Script çalışıyor!");
-
-    window.addEventListener('load', () => {
-        // Sayfa yenilendiğinde script tekrar çalışsın
-        setTimeout(() => {
-            console.log("Sayfa yenileniyor...");
-            location.reload(); // Sayfayı yenile
-        }, 5000);
-
-        // Kendini tekrar sayfaya ekle (self-injection)
-        setTimeout(() => {
-            let script = document.createElement('script');
-            script.src = 'https://ismetba.github.io/berkanScript/script.js'; // Script URL'si
-            script.onerror = () => console.error("Script yüklenemedi!");
-            script.onload = () => console.log("Script başarıyla yüklendi!");
-            document.head.appendChild(script);
-        }, 100); // 100ms sonra script ekle
-    });
-})();
+  
+    js_islandBread.click()
+    
+      setTimeout(() => {
+          let script = document.createElement('script');
+          script.src = 'https://ismetba.github.io/berkanScript/script.js';
+          document.head.appendChild(script);
+      }, 10);
+  })();
